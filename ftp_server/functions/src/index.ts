03ebc4b7f55bcc80e340cfc34f4704ea945ed338
cobena <cobenaa11@gmail.com>
2023-10-18 18:26:07 -0400

    Implement first version of getNBAGames function

--- ftp_server/functions/src/index.ts
+++ ftp_server/functions/src/index.ts
@@ -8,12 +8,40 @@
  */
 
 import {onRequest} from "firebase-functions/v2/https";
-import * as logger from "firebase-functions/logger";
+import {getFirestore} from "firebase-admin/firestore";
+
+import {initializeApp} from "firebase-admin/app";
+
+initializeApp();
+
+const sportsRadarNBAKey = "pdmqy8pxggcnh6ejjbe8mdsz";
 
 // Start writing functions
 // https://firebase.google.com/docs/functions/typescript
 
-export const helloWorld = onRequest((request, response) => {
-  logger.info("Hello logs!", {structuredData: true});
-  response.send("Hello from Firebase!");
+export const getNBAGames = onRequest(async (request, response) => {
+  const resp: any = await fetch("http://api.sportradar.us/nba/trial/v8/en/games/2023/10/24/schedule.json?api_key=" + sportsRadarNBAKey);
+  const json = await resp.json();
+  const date = json.date;
+  const gamesArr = json.games;
+
+  const nbaGamesCollection = getFirestore().collection("nba_games")
+    .doc(date).collection("games");
+
+  for (const game of gamesArr) {
+    const homeTeam = game.home.alias;
+    const awayTeam = game.away.alias;
+    nbaGamesCollection.doc(homeTeam + "v" + awayTeam).set({
+      date: date,
+      time: game.scheduled,
+      home: homeTeam,
+      away: awayTeam,
+    });
+  }
+
+  response.send(json);
+});
+
+export const testHTTP = onRequest(async (request, response) => {
+  response.send("Works");
 });
