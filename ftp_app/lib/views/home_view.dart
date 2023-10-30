e6b5da9c22a92dbb9e876cfe89c2dc4691ca2bf5
Ariel Cobena <cobenaa11@gmail.com>
2023-10-30 13:14:19 -0400

    Moved from main.dart

---
+++ ftp_app/lib/views/home_view.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+
+class SportsbookHomeScreen extends StatefulWidget {
+  @override
+  _SportsbookHomeScreenState createState() => _SportsbookHomeScreenState();
+}
+
+class _SportsbookHomeScreenState extends State<SportsbookHomeScreen> {
+  int _currentIndex = 0;
+
+  final List<Widget> _pages = [
+    HomeScreen(),
+    SettingsScreen(),
+    SocialFeedScreen(),
+    SimulatorScreen(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Sportsbook App'),
+        backgroundColor: Colors.blue,
+      ),
+      body: _pages[_currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: _currentIndex,
+        type: BottomNavigationBarType.fixed,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(Icons.home),
+            label: 'Home',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings),
+            label: 'Settings',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.rss_feed),
+            label: 'Social Feed',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(Icons.sports_esports),
+            label: 'Simulator',
+          ),
+        ],
+        onTap: (index) {
+          setState(() {
+            _currentIndex = index;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Home Screen'),
+    );
+  }
+}
+
+class SettingsScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Settings Screen'),
+    );
+  }
+}
+
+class SocialFeedScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Social Feed Screen'),
+    );
+  }
+}
+
+class SimulatorScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Text('Simulator Screen'),
+    );
+  }
+}
