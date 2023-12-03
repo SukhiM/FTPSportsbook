29bd3f943569d4f931fddc27835ddb99818c4401
cobena <cobenaa11@gmail.com>
2023-12-03 18:04:42 -0500

    Add share option to completed bets

--- ftp_app/lib/views/bet_history_view.dart
+++ ftp_app/lib/views/bet_history_view.dart
@@ -2,107 +2,177 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:intl/intl.dart';
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+
+const shareEndpoint = "https://sharebetslip-kca5bali4a-uc.a.run.app";
 
 String formatTimestamp(Timestamp timestamp) {
-  DateTime dateTime = timestamp.toDate(); // Convert to DateTime object
-
-  // Optionally, adjust the dateTime to the EST timezone
-  // DateTime estDateTime = dateTime.subtract(const Duration(hours: 5)); // EST is UTC-5
-
-  // For accurate timezone conversion considering Daylight Saving Time
-  // you might need to use a third-party package like 'timezone' to handle this properly
-  // For example:
-  // final easternTimeZone = getLocation('America/New_York');
-  // final estDateTime = TZDateTime.from(dateTime, easternTimeZone);
-
-  // Create a new DateFormat object and use it to format the DateTime object
+  DateTime dateTime = timestamp.toDate();
   DateFormat formatter = DateFormat('MM-dd-yyyy HH:mm');
-  return formatter.format(dateTime); // Format it to MM-dd-yyyy HH:mm
-}
-
-Future<String> createMatchupString(String gameID) async {
-  // This will perform a collection group query across all 'games' subcollections
-  QuerySnapshot querySnapshot = await FirebaseFirestore.instance
-      .collectionGroup('games')
-      .where(FieldPath.documentId, isEqualTo: gameID)
-      .get();
-
-  if (querySnapshot.docs.isNotEmpty) {
-    // Assuming the game document has 'team1' and 'team2' fields
-    Map<String, dynamic> gameData =
-        querySnapshot.docs.first.data() as Map<String, dynamic>;
-    String team1 = gameData['team1'];
-    String team2 = gameData['team2'];
-
-    // Create the matchup string
-    String matchup = "$team1 @ $team2";
-
-    return matchup;
-  } else {
-    // Handle the case where the game document does not exist
-    throw Exception('Game not found');
-  }
+  return formatter.format(dateTime);
 }
 
 class BetHistoryCard extends StatelessWidget {
   final String teamBetOn;
+  final String betID;
+  final String status;
   final double amount;
   final String matchup;
   final DateTime betPlacedDate;
 
   BetHistoryCard({
     required this.teamBetOn,
+    required this.betID,
+    required this.status,
     required this.amount,
     required this.matchup,
     required this.betPlacedDate,
   });
 
+  void _showShareDialog(BuildContext context, String betID) {
+    TextEditingController _messageController = TextEditingController();
+
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: Text('Share Bet Slip'),
+          content: TextField(
+            controller: _messageController,
+            decoration: InputDecoration(hintText: "Type your message here"),
+          ),
+          actions: <Widget>[
+            TextButton(
+              child: Text('Cancel'),
+              onPressed: () {
+                Navigator.of(context).pop();
+              },
+            ),
+            TextButton(
+              child: Text('Share'),
+              onPressed: () {
+                String message = _messageController.text;
+                _shareBetSlipToGlobal(context, betID, message);
+                Navigator.of(context).pop();
+              },
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  void _shareBetSlipToGlobal(
+      BuildContext context, String betID, String message) async {
+    String uid = FirebaseAuth.instance.currentUser!.uid;
+    ScaffoldMessengerState scaffoldMessenger = ScaffoldMessenger.of(context);
+
+    try {
+      final response = await http.post(
+        Uri.parse(shareEndpoint),
+        headers: <String, String>{
+          'Content-Type': 'application/json; charset=UTF-8',
+        },
+        body: jsonEncode(<String, dynamic>{
+          'uid': uid,
+          'betID': betID,
+          'message': message,
+        }),
+      );
+
+      if (response.statusCode == 200) {
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Bet shared successfully!')),
+        );
+      } else {
+        scaffoldMessenger.showSnackBar(
+          SnackBar(content: Text('Failed to share bet: ${response.body}')),
+        );
+      }
+    } catch (e) {
+      scaffoldMessenger.showSnackBar(
+        SnackBar(content: Text('Exception when sharing bet: $e')),
+      );
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
-    // For formatting date and time in a readable format
     final DateFormat dateFormat = DateFormat('MM-dd-yyyy');
     final DateFormat timeFormat = DateFormat('hh:mm a');
 
     return Card(
       margin: EdgeInsets.all(8.0),
-      child: Padding(
-        padding: EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            Text(
-              '$teamBetOn to Win',
-              style: TextStyle(
-                fontSize: 18.0,
-                fontWeight: FontWeight.bold,
+      child: Stack(
+        children: [
+          Padding(
+            padding: EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Text(
+                  '$teamBetOn to Win',
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  status,
+                  style: TextStyle(
+                    fontSize: 18.0,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  'Amount Wagered: \$${amount.toStringAsFixed(2)}',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    color: Colors.green,
+                  ),
+                ),
+                // SizedBox(height: 8),
+                // Text(
+                //   'Payout: ${payout.toStringAsFixed(2)}',
+                //   style: TextStyle(
+                //     fontSize: 16,
+                //     color: payout > 0 ? Colors.green : Colors.red,
+                //   ),
+                // ),
+                SizedBox(height: 8.0),
+                Text(
+                  '$matchup',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                    fontStyle: FontStyle.italic,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+                Text(
+                  '${dateFormat.format(betPlacedDate)} ${timeFormat.format(betPlacedDate)}',
+                  style: TextStyle(
+                    fontSize: 16.0,
+                  ),
+                ),
+                SizedBox(height: 8.0),
+              ],
+            ),
+          ),
+          if (status == 'won' ||
+              status ==
+                  'lost') // Share icon appears for completed ('won' or 'lost') bets only
+            Positioned(
+              right: 4.0,
+              bottom: 4.0,
+              child: IconButton(
+                icon: Icon(Icons.share),
+                onPressed: () => _showShareDialog(context, betID),
               ),
             ),
-            SizedBox(height: 8.0),
-            Text(
-              'Amount Wagered: \$${amount.toStringAsFixed(2)}',
-              style: TextStyle(
-                fontSize: 16.0,
-                color: Colors.green,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              '$matchup',
-              style: TextStyle(
-                fontSize: 16.0,
-                fontStyle: FontStyle.italic,
-              ),
-            ),
-            SizedBox(height: 8.0),
-            Text(
-              '${dateFormat.format(betPlacedDate)} ${timeFormat.format(betPlacedDate)}',
-              style: TextStyle(
-                fontSize: 16.0,
-              ),
-            ),
-            SizedBox(height: 8.0),
-          ],
-        ),
+        ],
       ),
     );
   }
@@ -125,11 +195,11 @@ class _BetHistoryViewState extends State<BetHistoryView> {
 
   Future<void> _fetchBets() async {
     try {
-      // Reference to the current user's betHistory subcollection
       QuerySnapshot betQuery = await FirebaseFirestore.instance
           .collection('users')
           .doc(_auth.currentUser!.uid)
           .collection('bet_history')
+          .orderBy('placedAt', descending: true)
           .get();
 
       setState(() {
@@ -137,7 +207,6 @@ class _BetHistoryViewState extends State<BetHistoryView> {
       });
     } catch (e) {
       print("Error fetching bets: $e");
-      // Show a message or handle the error
     }
   }
 
@@ -149,34 +218,17 @@ class _BetHistoryViewState extends State<BetHistoryView> {
         ),
         body: betList.isEmpty
             ? Center(child: CircularProgressIndicator())
-            // : ListView.builder(
-            //     itemCount: betList.length,
-            //     itemBuilder: (context, index) {
-            //       var bet = betList[index].data() as Map<String, dynamic>;
-            //       return Card(
-            //         margin: EdgeInsets.all(8.0),
-            //         child: ListTile(
-            //           title: Text('Match ID: ${bet['gameID']}'),
-            //           subtitle: Text('Amount: ${bet['amount']}'),
-            //           // trailing: Text('Status: ${bet['status']}'),
-            //           trailing: Text('${formatTimestamp(bet['placedAt'])}'),
-            //           // You can add more details or formatting as needed
-            //         ),
-            //       );
-            //     },
-            //   ),
             : ListView.builder(
-                itemCount: betList
-                    .length, // Assume this is the length of your bet history list
+                itemCount: betList.length,
                 itemBuilder: (context, index) {
-                  final bet =
-                      betList[index]; // Assuming this is a list of bet objects
+                  final bet = betList[index];
                   return BetHistoryCard(
                     teamBetOn: bet['team'],
+                    betID: bet.id,
+                    status: bet['status'] ?? "pending",
                     amount: bet['amount'].toDouble(),
                     matchup: bet['matchup'],
-                    betPlacedDate: bet['placedAt']
-                        .toDate(), // Make sure this is a DateTime object
+                    betPlacedDate: bet['placedAt'].toDate(),
                   );
                 },
               ));
