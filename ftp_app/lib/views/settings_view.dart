e672a7d7435c7bd186557ca1f3209736bd92df7a
Sukhi Singh <44445119+SukhiM@users.noreply.github.com>
2023-12-10 12:36:54 -0500

    Allows User to upload Profile Pictures and displays them on the Social Feed.

--- ftp_app/lib/views/settings_view.dart
+++ ftp_app/lib/views/settings_view.dart
@@ -1,3 +1,6 @@
+import 'dart:io';
+import 'package:image_picker/image_picker.dart';
+import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
@@ -14,6 +17,7 @@ class _SettingsViewState extends State<SettingsView> {
   final _auth = FirebaseAuth.instance;
   String? username;
   num? balance;
+  File? _imageFile;
 
   Future<void> _fetchBalance() async {
     try {
@@ -30,6 +34,37 @@ class _SettingsViewState extends State<SettingsView> {
     } catch (e) {
       // Handle the error or show a message
       print("Error fetching balance: $e");
+    }
+  }
+
+  Future<void> _pickImage() async {
+    final picker = ImagePicker();
+    final pickedFile = await picker.pickImage(source: ImageSource.gallery);
+
+    if (pickedFile != null) {
+      setState(() {
+        _imageFile = File(pickedFile.path);
+      });
+    }
+  }
+
+  Future<void> _uploadImage() async {
+    if (_imageFile != null) {
+      try {
+        var uid = FirebaseAuth.instance.currentUser?.uid;
+        var storageRef =
+        FirebaseStorage.instance.ref().child('profile_images/$uid.jpg');
+        await storageRef.putFile(_imageFile!);
+        var imageUrl = await storageRef.getDownloadURL();
+
+        // Save the image URL to Firestore under the user's document
+        await FirebaseFirestore.instance
+            .collection('users')
+            .doc(uid)
+            .update({'profileImageUrl': imageUrl});
+      } catch (e) {
+        print("Error uploading profile picture: $e");
+      }
     }
   }
 
@@ -50,6 +85,20 @@ class _SettingsViewState extends State<SettingsView> {
           ListTile(
             title: Text('Welcome'),
             subtitle: Text(username ?? 'N/A'),
+          ),
+          // Add a button to pick an image
+          ListTile(
+            title: ElevatedButton(
+              onPressed: _pickImage,
+              child: Text('Pick Profile Picture'),
+            ),
+          ),
+          // Add a button to upload the image
+          ListTile(
+            title: ElevatedButton(
+              onPressed: _uploadImage,
+              child: Text('Upload Profile Picture'),
+            ),
           ),
           ListTile(
             title: Text('Balance'),
